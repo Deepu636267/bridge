b24afea22b5da603902a03d498edb9f93cdd950f
Deepu636267 <deepumaurya07@gmail.com>
2019-10-05 12:00:59 +0530

    Functional Done

---
+++ Functional/StopWatch.cs
@@ -0,0 +1,41 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file=StopWatch.cs" company="Bridgelabz">
+//   Copyright © 2019 Company="BridgeLabz"
+// </copyright>
+// <creator name="Sachin Kumar Maurya"/>
+// --------------------------------------------------------------------------------------------------------------------
+namespace Functional
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+    class StopWatch
+    {
+        long startTimer;
+        long stopTimer;
+        long elapsed;
+        /// <summary>
+        /// Starts this instance start the stop watch.
+        /// </summary>
+        public void start()
+        {
+            startTimer = DateTime.Now.Millisecond; 
+        }
+        /// <summary>
+        /// Stops this instance stop the stopwatch.
+        /// </summary>
+        public void stop()
+        {
+            stopTimer = DateTime.Now.Millisecond; 
+        }
+        /// <summary>
+        /// Gets the elapsed time between start and stop.
+        /// </summary>
+        /// <returns></returns>
+        public int getElapsedTime()
+        {
+            elapsed = stopTimer - startTimer;
+            return (int)elapsed;
+        }
+    }
+}
---
+++ Functional/Utility.cs
@@ -0,0 +1,73 @@
+﻿
+// --------------------------------------------------------------------------------------------------------------------
+// <copyright file=Utility.cs" company="Bridgelabz">
+//   Copyright © 2019 Company="BridgeLabz"
+// </copyright>
+// <creator name="Sachin Kumar Maurya"/>
+// --------------------------------------------------------------------------------------------------------------------
+namespace Functional
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+    /// <summary>
+    /// Utility is class for code Reuseability
+    /// </summary>
+    class Utility
+    {
+        /// <summary>
+        /// Inputs the integer.
+        /// </summary>
+        /// <returns></returns>
+        public int inputInteger()
+        {
+            return Convert.ToInt32(Console.ReadLine());
+        }
+        /// <summary>
+        /// Inputs the string.
+        /// </summary>
+        /// <returns></returns>
+        public String inputString()
+        {
+            return Console.ReadLine();
+        }
+        /// <summary>
+        /// Inputs the double.
+        /// </summary>
+        /// <returns></returns>
+        public double inputDouble()
+        {
+            return Convert.ToDouble(Console.ReadLine());
+        }
+        /// <summary>
+        /// Inputs the bool.
+        /// </summary>
+        /// <returns></returns>
+       public bool inputBool()
+        {
+            return Convert.ToBoolean(Console.ReadLine());
+        }
+        /// <summary>
+        /// BubbleSort() method for Sorting the Integer type value
+        /// holds integer type as argument
+        /// </summary>
+        /// <param name="arr">The arr.</param>
+        /// <returns></returns>
+        public int[] bubbleSort(int[] arr)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                for (int j = 0; j < arr.Length - i - 1; j++)
+                {
+                    if (arr[j] > arr[j + 1])
+                    {
+                        int temp = arr[j];
+                        arr[j] = arr[j + 1];
+                        arr[j + 1] = temp;
+                    }
+                }
+            }
+            return arr;
+        }
+    }
+}
